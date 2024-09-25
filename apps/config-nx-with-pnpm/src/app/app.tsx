e67eab8b9e830f5b9747d8b19039cbf9e841f5b7
Reza Basereh <basereh@mahsan.co>
2024-09-25 16:48:29 +0330

    Initial commit

---
+++ apps/config-nx-with-pnpm/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="config-nx-with-pnpm" />
+    </div>
+  );
+}
+
+export default App;
