e797c2fb01b7447a05f99704021d91c01c28d533
Reza Basereh <basereh@mahsan.co>
2024-10-10 15:23:15 +0330

    install some dependencies

--- apps/host-app/webpack.config.ts
+++ apps/host-app/webpack.config.ts
@@ -1,7 +1,6 @@
 import { composePlugins, withNx } from '@nx/webpack';
 import { withReact } from '@nx/react';
 import { withModuleFederation } from '@nx/react/module-federation';
-
 import baseConfig from './module-federation.config';
 
 const config = {
